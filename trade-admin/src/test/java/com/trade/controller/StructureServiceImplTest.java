fc9bfb503452fa820d3991779069479133cab1b6
ben <ben.huang@aax.com>
2021-03-18 15:20:54 +0800

    :white_check_mark: 添加一个稀疏数组转二维数组功能测试用例

--- trade-admin/src/test/java/com/trade/controller/StructureServiceImplTest.java
+++ trade-admin/src/test/java/com/trade/controller/StructureServiceImplTest.java
@@ -15,11 +15,26 @@ public class StructureServiceImplTest {
     @Test
     public void twoDimensionalArrayToSparseArray(){
         StrutureServiceImpl strutureService = new StrutureServiceImpl();
-        int arr[][] = {{0 , 0 , 1 , 0 , 0},{0 , 2 , 0 , 0 , 0},{0 , 0 , 0 , 3 , 0}};
+        int[][] arr = {{0 , 0 , 1 , 0 , 0},{0 , 2 , 0 , 0 , 0},{0 , 0 , 0 , 3 , 0}};
         int[][] ints = strutureService.twoDimensionalArrayToSparseArray(arr);
 
-        for (int i = 0; i < ints.length; i++) {
-                System.out.printf("%d\t%d\t%d\t\n",ints[i][0],ints[i][1],ints[i][2]);
+        for (int[] anInt : ints) {
+            System.out.printf("%d\t%d\t%d\t\n", anInt[0], anInt[1], anInt[2]);
         }
     }
+
+    @Test
+    public void sparseArrayTotwoDimensionalArray(){
+        StrutureServiceImpl strutureService = new StrutureServiceImpl();
+        int[][] arr = {{3 , 5 , 3},{0 , 2 , 1},{1 , 1 , 2},{2, 3 , 3}};
+        int[][] ints = strutureService.sparseArrayTotwoDimensionalArray(arr);
+
+       for (int i = 0;i < ints.length;i++){
+           for (int j = 0;j < ints[i].length;j++){
+               System.out.printf("%d\t",ints[i][j]);
+           }
+           System.out.println();
+       }
+    }
+
 }
