29dc34bb3e8505977516df5df6e11f9b555e104b
ben <ben.huang@aax.com>
2021-03-18 18:59:25 +0800

    :white_check_mark:添加注释记录

--- trade-admin/src/main/java/com/trade/utils/ApiUtils.java
+++ trade-admin/src/main/java/com/trade/utils/ApiUtils.java
@@ -8,7 +8,11 @@ public class ApiUtils {
 
     /**
      * 静态代码块的意义在于在程序运行之前直接执行代码块里运行
-     * 类加载的过程 BootStrapClassLoader (C++) -> ExtClassLoader  -> AppClassLoader
+     * 1.类加载的过程 BootStrapClassLoader (C++) -> ExtClassLoader  -> AppClassLoader
+     * 每种类加载起都有自己的加载目录
+     * 2.双亲委派机制
+     *   ① 每个类加载器对他加载过的类都有一个缓存。
+     *   ② 向上委托查找，向下委托加载.
      */
     static {
         System.out.println("Hello! This is for Api trainning");
