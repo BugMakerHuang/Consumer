c65fe7563ba7e887c8c18d2bd37a2d0752639011
ben <ben.huang@aax.com>
2021-03-18 19:05:08 +0800

    :white_check_mark:添加注释记录

--- trade-admin/src/main/java/com/trade/utils/ApiUtils.java
+++ trade-admin/src/main/java/com/trade/utils/ApiUtils.java
@@ -8,8 +8,9 @@ public class ApiUtils {
 
     /**
      * 静态代码块的意义在于在程序运行之前直接执行代码块里运行
-     * 1.类加载的过程 BootStrapClassLoader (C++) -> ExtClassLoader  -> AppClassLoader
-     * 每种类加载起都有自己的加载目录
+     * 1.JAVA的类加载的过程
+     *    BootStrapClassLoader (C++) -> ExtClassLoader  -> AppClassLoader
+     *    每种类加载起都有自己的加载目录
      * 2.双亲委派机制
      *   ① 每个类加载器对他加载过的类都有一个缓存。
      *   ② 向上委托查找，向下委托加载.
